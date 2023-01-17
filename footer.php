5fa0649f10be111fcb1481bf98e67706f3d4e067
Giang <minhgiang20011997@gmail.com>
2023-01-18 01:03:38 +0700

    add footer

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer class="footer">
+    <section class="box-container">
+        <div class="box">
+            <h3>quick link</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+            <a href="shop.php"><i class="fas fa-angle-right"></i> shop</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+            <a href="contact.php"><i class="fas fa-angle-right"></i> contact</a>
+        </div>
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="cart.php"><i class="fas fa-angle-right"></i> cart</a>
+            <a href="wishlist.php"><i class="fas fa-angle-right"></i> wishlist</a>
+            <a href="login.php"><i class="fas fa-angle-right"></i> login</a>
+            <a href="register.php"><i class="fas fa-angle-right"></i> register</a>
+        </div>
+        <div class="box">
+            <h3>quick link</h3>
+            <p href="home.php"> <i class="fas fa-phone"></i> +84974045967</p>
+            <p href="home.php"> <i class="fas fa-phone"></i> +84974045967</p>
+            <p href="about.php"><i class="fas fa-envelope"></i> minhgiang@gmail.com</p>
+            <p href="contact.php"><i class="fas fa-map-marker-alt"></i> Ha Noi</p>
+        </div>
+        <div class="box">
+            <h3>folow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i> instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i> linkedin</a>
+        </div>
+    </section>
+</footer>
